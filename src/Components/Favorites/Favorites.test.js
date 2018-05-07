30334554d744f9f6c39d81fb41831064ea270a17
Daniela Carey <danielafcarey@gmail.com>
2018-05-07 16:21:57 -0600

    Setup initial component files, test files, and scss files

---
+++ src/Components/Favorites/Favorites.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Favorites from './Favorites';
+
+it('renders without crashing', () => {
+  // shallow(<Favorites />);
+});
---
+++ src/Components/Landing/Landing.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Landing from './Landing';
+
+it('renders without crashing', () => {
+  // shallow(<Landing />);
+});
